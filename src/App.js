1e779b547e0e4dc4de28441876e99535c94a87a3
Mariana Rascon <51216146+Nienorloth@users.noreply.github.com>
2020-04-05 20:13:29 -0500

    header and form started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Header from './components/Header.js';
+import Formulario from './components/Formulario.js';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+max-width: 600px;
+margin: 0 auto;
+`
+const ContenedorFormulario =styled.div`
+background-color: #FFF;
+padding: 3rem;
+
+`
 
 function App() {
   return (
+    <Contenedor>
+      <Header
+        titulo='Cotizador de seguros'
+      />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
